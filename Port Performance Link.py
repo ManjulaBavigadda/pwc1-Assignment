783d2c5cdaff2ef8dd94e640bf2cf82be8989f9e
ManjulaBavigadda <manjulabavigadda@gmail.com>
2025-06-18 13:44:32 +0530

    Create Port Performance Link.py

---
+++ Port Performance Link.py
@@ -0,0 +1,14 @@
+{
+  "linkType": "HasPortPerformance",
+  "objectTypeA": "Economy",
+  "objectTypeB": "PortPerformance",
+  "cardinality": "ONE_TO_MANY"
+}
+
+# Commercial Market measured in Port Performance Link
+{
+  "linkType": "MeasuredIn",
+  "objectTypeA": "CommercialMarket",
+  "objectTypeB": "PortPerformance",
+  "cardinality": "ONE_TO_MANY"
+}
