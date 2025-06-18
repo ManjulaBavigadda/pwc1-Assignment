86bca7c351f5a35a8e8e0649bafdce550707f95f
ManjulaBavigadda <manjulabavigadda@gmail.com>
2025-06-18 13:42:11 +0530

    Create Economy Object Type.py

---
+++ Economy Object Type.py
@@ -0,0 +1,10 @@
+
+  "objectType": "Economy",
+  "properties": {
+    "economy_code": "string",
+    "economy_name": "string",
+    "region": "string",
+    "development_status": "string"
+  },
+  "primaryKey": "economy_code"
+}
