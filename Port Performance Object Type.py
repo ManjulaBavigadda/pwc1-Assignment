5df7897a134e119e4dc3b2f8f5efd71ed4ede201
ManjulaBavigadda <manjulabavigadda@gmail.com>
2025-06-18 13:43:16 +0530

    Create Port Performance Object Type.py

---
+++ Port Performance Object Type.py
@@ -0,0 +1,11 @@
+{
+  "objectType": "PortPerformance",
+  "properties": {
+    "median_time_in_port_days": "double",
+    "average_vessel_age_years": "double",
+    "average_size_gt_vessels": "double",
+    "maximum_size_gt_vessels": "double",
+    "reporting_period": "string"
+  },
+  "primaryKey": "performance_id"
+}
